90c726ee5ac50ba4f9676547e48087bd9666b2c1
saent-x <vangerwua@outlook.com>
2021-04-09 20:27:56 +0100

    Added company link at bottom

--- components/BottomInfo.js
+++ components/BottomInfo.js
@@ -1,11 +1,37 @@
-import { Text } from "@chakra-ui/layout";
+import { Text, Center } from "@chakra-ui/layout";
 
 export default function BottomInfo() {
     
-    return (
-      <Text w="100%" fontSize="sm" ml="5px" mr="5px" mt="10px" mb="20px" textAlign="center" letterSpacing="widest">
+  return (
+    <>
+      <Text
+        w="100%"
+        fontSize="sm"
+        ml="5px"
+        mr="5px"
+        mt="10px"
+        mb="10px"
+        textAlign="center"
+        letterSpacing="widest"
+      >
         Expressforecast.com is not connected to or affiliated by any of the
         lotteries, organisations, or any bodies listed on this site.
       </Text>
-    );
+      <a href="http://www.scitylanang.com/">
+        <Text
+          w="100%"
+          fontSize="sm"
+          ml="5px"
+          mr="5px"
+          mt="10px"
+          mb="5px"
+          textAlign="center"
+          letterSpacing="widest"
+          textDecor="underline"
+        >
+          powered by scitylana
+        </Text>
+      </a>
+    </>
+  );
 }
